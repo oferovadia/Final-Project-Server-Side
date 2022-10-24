95fe14247e9918408d8aa15bb674fb87692bfa5c
oferovadia <ovadiaofer@gmail.com>
2022-10-24 14:33:59 +0300

    changes

--- src/cart/dto/create-cart.dto.ts
+++ src/cart/dto/create-cart.dto.ts
@@ -1,1 +1,10 @@
-export class CreateCartDto {}
+import { IsNotEmpty, IsNumber, IsOptional } from 'class-validator';
+
+export class CreateCartDto {
+  @IsOptional()
+  @IsNumber()
+  id: number;
+
+  @IsNotEmpty()
+  customer_id: number;
+}
